fa4a8f431ab33551bac1c0e1ad767c19f5fe4121
MikleAKA <mixa1410buz@gmail.com>
2024-05-27 00:21:37 +0300

    fixes

--- src/components/Review-Form.tsx
+++ src/components/Review-Form.tsx
@@ -71,9 +71,9 @@ export default function ReviewForm({offerId}: ReviewFormProps): JSX.Element {
       <textarea className="reviews__textarea form__textarea" id="review" name="review" value={reviewText} onChange={handleReviewText} placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-          To submit review please sign in, make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          To submit review please sign in, make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters (but not more than 300 characters)</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={rating === '' || reviewText.length < 50 || isAuthorized !== AuthorizationStatus.Auth} >Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={rating === '' || reviewText.length < 50 || isAuthorized !== AuthorizationStatus.Auth || reviewText.length > 300} >Submit</button>
       </div>
     </form>
   );
